d38dd96c1b0aa35ab05f48cf28d4af4a8dec9437
vagrant <vagrant@homestead>
2018-11-16 07:05:44 +0000

    Finish user signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    public function gravatar($size = "100"){
+	return "https://iocaffcdn.phphub.org/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34";
+    	#$hash = md5(strtolower(trim($this->attributes("email"))));
+	#return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
